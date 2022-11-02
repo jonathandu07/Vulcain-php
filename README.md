94689d1d6b375cc957b2fa3f32a5f4603057a268
jonathandu07 <alphajorider@gmail.com>
2022-11-02 18:38:17 +0100

    mise en place

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vulcain
+ copy of my symfony web application, of the same name.
+
+to build the style : npm run wacth:scss
---
+++ components/_disconnected-navbar.php
@@ -0,0 +1,25 @@
+<nav class="nav">
+    <div class="nav__div-logo">
+        <img src="/src/Vulcain-BG.png" alt="logo" onclick="history.back()">
+    </div>
+    <div class="nav__disconnected">
+        <div class="nav__disconnected__div-left">
+            <a href="" class="header__nav-right__div-left__a">
+                <img src="/src/spain.png" alt="espagne" class="">
+            </a>
+        </div>
+        <div class="nav__disconnected__div-middle">
+            <a href="" class="header__nav-right__div-middle__a">
+                <img src="/src/united_kingdom.png" alt="angleterre" class="">
+            </a>
+        </div>
+        <div class="nav__disconnected__div-right">
+            <a href="" class="nav__disconnected__div-right__a">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                </svg>
+            </a>
+        </div>
+    </div>
+</nav>
